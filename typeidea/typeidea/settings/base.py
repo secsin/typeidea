c8acb45c3568ce594b39bd729583890babce4b94
secsin <32921301+secsin@users.noreply.github.com>
2022-06-08 17:19:47 +0800

    完成models模块

--- typeidea/typeidea/settings/base.py
+++ typeidea/typeidea/settings/base.py
@@ -36,6 +36,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog',
+    'config',
+    'comment',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
